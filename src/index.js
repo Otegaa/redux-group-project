39ea4558952df44cb9b20afb10e8bd642c04d963
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-05-15 14:09:56 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './components/App';
+import Store from './features/store';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={Store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
-
