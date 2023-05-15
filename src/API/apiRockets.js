a5fac4dfa0f430fb58a303a4dbff4d03a3e72fe1
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-05-15 14:08:06 +0100

    Connect to the rockets API

---
+++ src/API/apiRockets.js
@@ -0,0 +1,17 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const BaseURL = 'https://api.spacexdata.com/v4/rockets';
+
+export const fetchRockets = createAsyncThunk('rockets/fetchRockets', async () => {
+  const response = await axios.get(BaseURL);
+  const rockets = response.data;
+  return rockets.map((rocket) => (
+    {
+      id: rocket.id,
+      name: rocket.name,
+      description: rocket.description,
+      image: rocket.flickr_images[0],
+    }
+  ));
+});
