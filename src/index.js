f85ce90a709d9f60ac42ef6115bcf6d994b957fe
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-05-16 09:17:16 +0100

    Imported the styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import Store from './features/store';
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
